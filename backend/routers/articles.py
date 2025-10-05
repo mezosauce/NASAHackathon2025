ef1dd59a95294f41c33baf122c74a9f7297a28bf
djcadon <caden1215@ipmolders.com>
2025-10-05 05:00:56 -0400

    fixed /api/articles endpoint

--- backend/routers/articles.py
+++ backend/routers/articles.py
@@ -1,5 +1,14 @@
 from fastapi import APIRouter, Query
-from db.client import supabase
+from supabase import create_client, Client 
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")   
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)    
 
 router = APIRouter(prefix="/api/articles", tags=["System"])
 
